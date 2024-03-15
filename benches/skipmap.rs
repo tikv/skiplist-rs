2ac6145c2c2b332a27d6b458d97d2ce17c73cdf2
SpadeA-Tang <u6748471@anu.edu.au>
2024-03-15 13:32:18 +0800

    done
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- benches/skipmap.rs
+++ benches/skipmap.rs
@@ -1,101 +1,93 @@
 #![feature(test)]
-#![allow(clippy::unit_arg)]
 
 extern crate test;
 
 use test::{black_box, Bencher};
 
-use crossbeam_epoch as epoch;
-use crossbeam_skiplist::SkipList;
+use crossbeam_skiplist::SkipMap as Map;
 
 #[bench]
 fn insert(b: &mut Bencher) {
-    let guard = &epoch::pin();
-
     b.iter(|| {
-        let map = SkipList::new(epoch::default_collector().clone());
+        let map = Map::new();
 
         let mut num = 0u64;
         for _ in 0..1_000 {
             num = num.wrapping_mul(17).wrapping_add(255);
-            map.insert(num, !num, guard);
+            map.insert(num, !num);
         }
     });
 }
 
 #[bench]
 fn iter(b: &mut Bencher) {
-    let guard = &epoch::pin();
-    let map = SkipList::new(epoch::default_collector().clone());
+    let map = Map::new();
 
     let mut num = 0u64;
     for _ in 0..1_000 {
         num = num.wrapping_mul(17).wrapping_add(255);
-        map.insert(num, !num, guard).release(guard);
+        map.insert(num, !num);
     }
 
     b.iter(|| {
-        for x in map.iter(guard) {
-            black_box(x.key());
+        for x in map.iter() {
+            black_box(x);
         }
     });
 }
 
 #[bench]
 fn rev_iter(b: &mut Bencher) {
-    let guard = &epoch::pin();
-    let map = SkipList::new(epoch::default_collector().clone());
+    let map = Map::new();
 
     let mut num = 0u64;
     for _ in 0..1_000 {
         num = num.wrapping_mul(17).wrapping_add(255);
-        map.insert(num, !num, guard).release(guard);
+        map.insert(num, !num);
     }
 
     b.iter(|| {
-        for x in map.iter(guard).rev() {
-            black_box(x.key());
+        for x in map.iter().rev() {
+            black_box(x);
         }
     });
 }
 
 #[bench]
 fn lookup(b: &mut Bencher) {
-    let guard = &epoch::pin();
-    let map = SkipList::new(epoch::default_collector().clone());
+    let map = Map::new();
 
     let mut num = 0u64;
     for _ in 0..1_000 {
         num = num.wrapping_mul(17).wrapping_add(255);
-        map.insert(num, !num, guard).release(guard);
+        map.insert(num, !num);
     }
 
     b.iter(|| {
         let mut num = 0u64;
+
         for _ in 0..1_000 {
             num = num.wrapping_mul(17).wrapping_add(255);
-            black_box(map.get(&num, guard));
+            black_box(map.get(&num));
         }
     });
 }
 
 #[bench]
 fn insert_remove(b: &mut Bencher) {
-    let guard = &epoch::pin();
-
     b.iter(|| {
-        let map = SkipList::new(epoch::default_collector().clone());
+        let map = Map::new();
 
         let mut num = 0u64;
         for _ in 0..1_000 {
             num = num.wrapping_mul(17).wrapping_add(255);
-            map.insert(num, !num, guard).release(guard);
+            map.insert(num, !num);
         }
 
         let mut num = 0u64;
         for _ in 0..1_000 {
             num = num.wrapping_mul(17).wrapping_add(255);
-            black_box(map.remove(&num, guard).unwrap().release(guard));
+            black_box(map.remove(&num).unwrap());
         }
     });
 }
